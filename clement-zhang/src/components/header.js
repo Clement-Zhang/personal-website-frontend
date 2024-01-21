1b0c15b1c5c5477ac21299fb53e1db5ab4f750ff
Clement-Zhang <zhangclement947@yahoo.ca>
2024-01-20 20:50:30 -0500

    home page

---
+++ clement-zhang/src/components/header.js
@@ -0,0 +1,12 @@
+import MyNavbar from "./navbar";
+
+const Header = () => {
+    return (
+        <div>
+            <h3>Clement</h3>
+            <MyNavbar />
+        </div>
+    );
+}
+
+export default Header;
--- clement-zhang/src/pages/home.js
+++ clement-zhang/src/pages/home.js
@@ -1,10 +1,15 @@
-import MyNavbar from "../components/navbar";
+import Template from "./template";
 
 const Home = () => {
     return (
         <div>
-            <h1>Clement Zhang</h1>
-            <MyNavbar />
+            <Template />
+            <p>
+                Hello there! I'm Clement Zhang, a Computer Science student at Toronto Metropolitan University 
+                (formerly Ryerson University). I'm interested in software engineering and web development. 
+                I'm currently looking for a 2024 summer internship. I made this website to showcase my skills in 
+                React. In the future, I will improve the pages and add more content.
+            </p>
         </div>
     );
 }
