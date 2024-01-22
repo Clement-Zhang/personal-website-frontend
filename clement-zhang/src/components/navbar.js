004970176ffd2af280dea0f48542d1932f725c74
Clement-Zhang <zhangclement947@yahoo.ca>
2024-01-22 12:05:01 -0500

    add some styling

--- clement-zhang/src/components/navbar.js
+++ clement-zhang/src/components/navbar.js
@@ -1,12 +1,11 @@
-import Navbar from "react-bootstrap/Navbar";
+import { Navbar} from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Nav from 'react-bootstrap/Nav';
 
 const MyNavbar = () => {
     return (
-        <Navbar >
+        <Navbar>
             <Navbar.Brand><Link to="/">About Me</Link></Navbar.Brand>
-            <Nav.Link><Link to="/projects">Projects</Link></Nav.Link>
+            <Link to="/projects">My Projects</Link>
         </Navbar>
     );
 }
