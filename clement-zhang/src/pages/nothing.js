81a99e296f5d7696908ec4c326c107fcd93c06b9
Clement-Zhang <zhangclement947@yahoo.ca>
2024-01-25 20:51:01 -0500

    img as nothing page instead of text

--- clement-zhang/src/pages/nothing.js
+++ clement-zhang/src/pages/nothing.js
@@ -1,12 +1,12 @@
 import Default from "../templates/default";
+import img from "../assets/under_construction.jpg";
+import styles from "../styles/nothing.module.css";
 
 const Nothing = () => {
     return (
         <div>
             <Default>
-                <p>
-                    Nothing here yet.
-                </p>
+                <img src={img} alt="Under Construction" className={styles.img}/>
             </Default>
         </div>
     );
