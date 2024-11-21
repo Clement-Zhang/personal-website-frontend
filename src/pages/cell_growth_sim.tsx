f679adfb73f8e4a3abe76a570690fea6937f05ad
Clement-Zhang <zhangclement947@yahoo.ca>
2024-11-20 19:04:40 -0500

    manually put and remove cells

--- src/pages/cell_growth_sim.tsx
+++ src/pages/cell_growth_sim.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useState, useRef } from "react";
 require("../styles/cell_growth_sim.module.css");
 
 const play = require("../assets/play_button.jpg")
@@ -6,16 +6,6 @@ const pause = require("../assets/pause_button.jpg")
 const reset = require("../assets/reset_button.jpg")
 
 const Sim = () => {
-    function change_button(e: SyntheticEvent) {
-        let element = e.target as HTMLImageElement
-        if (element.getAttribute("data-active-button") === "play") {
-            element.setAttribute("data-active-button", "pause")
-            element.src = pause
-        } else {
-            element.setAttribute("data-active-button", "play")
-            element.src = play
-        }
-    }
     const [controls, change_controls] = useState({ "div_time": 1, "div_fail_rate": 0, "cell_life": 1 });
     const adjust = (e: SyntheticEvent) => {
         const target = e.target as typeof e.target & {
@@ -26,15 +16,50 @@ const Sim = () => {
         const value = target.value;
         change_controls(values => ({ ...values, [name]: value }))
     }
+    const canvas_ref = useRef<HTMLCanvasElement | null>(null);
+    const [mouse, move_mouse] = useState([0, 0]);
+    function change_button(e: SyntheticEvent) {
+        let element = e.target as HTMLImageElement
+        if (element.getAttribute("data-active-button") === "play") {
+            element.setAttribute("data-active-button", "pause")
+            element.src = pause
+        } else {
+            element.setAttribute("data-active-button", "play")
+            element.src = play
+        }
+    }
+    function reset_button(e: SyntheticEvent) {
+        let element = document.getElementById("play_pause") as HTMLImageElement
+        element.setAttribute("data-active-button", "play")
+        element.src = play
+    }
+    function update_mouse(e: React.MouseEvent<HTMLCanvasElement>) {
+        move_mouse([e.clientX - (canvas_ref.current as HTMLCanvasElement).getBoundingClientRect().left, e.clientY - (canvas_ref.current as HTMLCanvasElement).getBoundingClientRect().top])
+    }
+    function interact(e: React.MouseEvent<HTMLCanvasElement>) {
+        e.preventDefault()
+        let ctx = (canvas_ref.current as HTMLCanvasElement).getContext("2d") as CanvasRenderingContext2D
+        let x = mouse[0] - (mouse[0] % 2)
+        let y = mouse[1] - (mouse[1] % 2)
+        if (e.button === 0) {
+            put_cell(x, y, ctx)
+        } else {
+            ctx.clearRect(x, y, 2, 2)
+        }
+    }
+    function put_cell(x: number, y: number, ctx: CanvasRenderingContext2D) {
+        ctx.fillStyle = "green"
+        ctx.fillRect(x, y, 2, 2)
+    }
     return (
-        <div style={{display: "flex"}}>
-            <div style={{flex: "50%"}}>
+        <div style={{ display: "flex" }}>
+            <div style={{ flex: "50%" }}>
                 <ul>
                     <li>
-                        <img src={play} alt="play button" data-active-button="play" onClick={change_button}/>
+                        <img id="play_pause" src={play} alt="play button" data-active-button="play" onClick={change_button} />
                     </li>
                     <li>
-                        <img src={reset} alt="reset button"/>
+                        <img src={reset} alt="reset button" onClick={reset_button} />
                     </li>
                 </ul>
                 <form>
@@ -52,8 +77,10 @@ const Sim = () => {
                     <input type="number" name="cell_life" min={1} value={controls.cell_life || 1} onChange={adjust} />
                 </form>
             </div>
-            <div style={{flex: "50%"}}>
-                <p>test</p>
+            <div style={{ flex: "50%" }}>
+                <canvas ref={canvas_ref} width={400} height={400} onMouseMove={update_mouse} onClick={interact} onContextMenu={interact}>
+
+                </canvas>
             </div>
         </div>
     );
