a5823237d51f0b2d3514690cf4120b6c9d16af52
Clement-Zhang <zhangclement947@yahoo.ca>
2024-11-23 19:34:47 -0500

    mvp

--- src/pages/cell_growth_sim.tsx
+++ src/pages/cell_growth_sim.tsx
@@ -8,16 +8,24 @@ const reset = require("../assets/reset_button.jpg")
 const Sim = () => {
     class Cell {
         create_time: number
-        constructor() {
-            this.create_time = Date.now()
+        cycle: number
+        lifetime: number | undefined
+        div_time: number | undefined
+        div_fail_rate: number | undefined
+        constructor(create_time: number, lifetime: number, div_time: number, div_fail_rate: number) {
+            this.lifetime = lifetime
+            this.div_time = div_time
+            this.div_fail_rate = div_fail_rate
+            this.create_time = create_time
+            this.cycle = 0
         }
     }
     class Dish {
         cells: (Cell | null)[][]
         ctx: CanvasRenderingContext2D
-        lifetime: number | undefined
-        div_time: number | undefined
-        div_fail_rate: number | undefined
+        elapsed_time: number
+        prev_frame_time: number | undefined
+        prev_frame_id: number | undefined
         constructor(ctx: CanvasRenderingContext2D) {
             let tmp: (Cell | null)[][] = []
             for (let i = 0; i < 200; i++) {
@@ -29,20 +37,33 @@ const Sim = () => {
             }
             this.cells = tmp
             this.ctx = ctx
+            this.elapsed_time = 0
         }
-        set(lifetime: number, div_time: number, div_fail_rate: number) {
-            this.lifetime = lifetime
-            this.div_time = div_time
-            this.div_fail_rate = div_fail_rate
-            console.log("lifetime: " + this.lifetime, "div_time: " + this.div_time, "div_fail_rate: " + this.div_fail_rate)
+        play() {
+            this.prev_frame_time = Date.now()
+            this.prev_frame_id = requestAnimationFrame(this.frame.bind(this))
         }
-        interact(e: React.MouseEvent<HTMLCanvasElement>, mouse: number[]) {
+        pause() {
+            this.elapsed_time += Date.now() - this.prev_frame_time!
+            cancelAnimationFrame(this.prev_frame_id!)
+        }
+        resume() {
+            this.prev_frame_time = Date.now()
+            this.prev_frame_id = requestAnimationFrame(this.frame.bind(this))
+        }
+        reset() {
+            cancelAnimationFrame(this.prev_frame_id!)
+            this.elapsed_time = 0
+            this.cells = this.cells.map(row => row.map(cell => null))
+            this.ctx.clearRect(0, 0, 400, 400)
+        }
+        interact(e: React.MouseEvent<HTMLCanvasElement>, mouse: number[], lifetime: number, div_time: number, div_fail_rate: number) {
             e.preventDefault()
             let x = Math.floor(mouse[0] / 2)
             let y = Math.floor(mouse[1] / 2)
             if (!this.cells[y][x]) {
                 if (e.button === 0) {
-                    this.create_cell(x, y)
+                    this.create_cell(x, y, lifetime, div_time, div_fail_rate)
                     this.ctx.fillRect(x * 2, y * 2, 2, 2)
                 } else {
                     this.delete_cell(x, y)
@@ -50,37 +71,38 @@ const Sim = () => {
                 }
             }
         }
-        create_cell(x: number, y: number) {
-            this.cells[y][x] = new Cell()
+        create_cell(x: number, y: number, lifetime: number, div_time: number, div_fail_rate: number) {
+            this.cells[y][x] = new Cell(this.elapsed_time, lifetime, div_time, div_fail_rate)
         }
         delete_cell(x: number, y: number) {
             this.cells[y][x] = null
         }
         frame(timestamp: number) {
-            let time = Date.now()
+            this.elapsed_time += Date.now() - this.prev_frame_time!
             for (let y = 0; y < 200; y++) {
                 for (let x = 0; x < 200; x++) {
                     if (this.cells[y][x]) {
-                        if (time - this.cells[y][x]!.create_time >= this.lifetime!) {
+                        if (this.elapsed_time - this.cells[y][x]!.create_time >= this.cells[y][x]!.lifetime!) {
                             this.delete_cell(x, y)
-                        } else if (time - this.cells[y][x]!.create_time >= this.div_time! && Math.random() > this.div_fail_rate!) {
+                        } else if (Math.floor((this.elapsed_time - this.cells[y][x]!.create_time) / this.cells[y][x]!.div_time!) > this.cells[y][x]!.cycle && Math.random() > this.cells[y][x]!.div_fail_rate!) {
                             let squares = []
-                            if (x > 0 && !this.cells[x - 1][y]) {
+                            if (x > 0 && !this.cells[y][x - 1]) {
                                 squares.push([x - 1, y])
                             }
-                            if (x < 199 && !this.cells[x + 1][y]) {
+                            if (x < 199 && !this.cells[y][x + 1]) {
                                 squares.push([x + 1, y])
                             }
-                            if (y > 0 && !this.cells[x][y - 1]) {
+                            if (y > 0 && !this.cells[y - 1][x]) {
                                 squares.push([x, y - 1])
                             }
-                            if (y < 199 && !this.cells[x][y + 1]) {
+                            if (y < 199 && !this.cells[y + 1][x]) {
                                 squares.push([x, y + 1])
                             }
                             if (squares.length > 0) {
                                 let [new_x, new_y] = squares[Math.floor(Math.random() * squares.length)]
-                                this.create_cell(new_x, new_y)
+                                this.create_cell(new_x, new_y, this.cells[y][x]!.lifetime!, this.cells[y][x]!.div_time!, this.cells[y][x]!.div_fail_rate!)
                             }
+                            this.cells[y][x]!.cycle = Math.floor((this.elapsed_time - this.cells[y][x]!.create_time) / this.cells[y][x]!.div_time!)
                         }
                     }
                 }
@@ -97,7 +119,8 @@ const Sim = () => {
                     }
                 }
             }
-            requestAnimationFrame(this.frame.bind(this))
+            this.prev_frame_time = Date.now()
+            this.prev_frame_id = requestAnimationFrame(this.frame.bind(this))
         }
     }
     const [controls, change_controls] = useState({
@@ -114,8 +137,9 @@ const Sim = () => {
         const value = target.value;
         change_controls(values => ({ ...values, [name]: value }))
     }
-    const canvas_ref = useRef<HTMLCanvasElement | null>(null);
-    const [mouse, update_mouse] = useState([0, 0]);
+    let canvas_ref = useRef<HTMLCanvasElement | null>(null);
+    let mouse = useRef<[number, number]>([0, 0]);
+    let animation = useRef<boolean>(false);
     let cells = useRef<Dish | null>(null);
     useEffect(() => {
         let ctx = canvas_ref.current!.getContext("2d")
@@ -127,25 +151,32 @@ const Sim = () => {
         if (element.getAttribute("data-active-button") === "play") {
             element.setAttribute("data-active-button", "pause")
             element.src = pause
-            cells.current!.set(controls.cell_life!, controls.div_time!, controls.div_fail_rate!)
-            requestAnimationFrame(cells.current!.frame.bind(cells.current!))
+            if (animation.current) {
+                cells.current!.resume()
+            } else {
+                cells.current!.play()
+                animation.current = true
+            }
         } else {
             element.setAttribute("data-active-button", "play")
             element.src = play
+            cells.current!.pause()
         }
     }
     function reset_button(e: SyntheticEvent) {
         let element = document.getElementById("play_pause") as HTMLImageElement
         element.setAttribute("data-active-button", "play")
         element.src = play
+        animation.current = false
+        cells.current!.reset()
     }
     function move_mouse(e: React.MouseEvent<HTMLCanvasElement>) {
         if (canvas_ref.current) {
-            update_mouse([e.clientX - canvas_ref.current.getBoundingClientRect().left, e.clientY - canvas_ref.current.getBoundingClientRect().top])
+            mouse.current = [e.clientX - canvas_ref.current.getBoundingClientRect().left, e.clientY - canvas_ref.current.getBoundingClientRect().top]
         }
     }
     function interact(e: React.MouseEvent<HTMLCanvasElement>) {
-        cells.current!.interact(e, mouse)
+        cells.current!.interact(e, mouse.current, controls.cell_life! * 1000, controls.div_time! * 1000, controls.div_fail_rate!)
     }
     return (
         <div style={{ display: "flex" }}>
