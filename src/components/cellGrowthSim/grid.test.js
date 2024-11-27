7fb8418f0328f2c2399f646ca90b39739f94e014
Clement-Zhang <zhangclement947@yahoo.ca>
2024-11-27 18:24:12 -0500

    bugfix

--- src/components/cellGrowthSim/grid.test.js
+++ src/components/cellGrowthSim/grid.test.js
@@ -3,10 +3,15 @@ import Grid from "./grid"
 import { Settings, Test } from "./constants"
 
 let grid
-let count = 0
+let func
 
 beforeEach(() => {
     grid = new Grid()
+    func = Math.random
+})
+
+afterEach(() => {
+    Math.random = func
 })
 
 test("initialize", () => {
@@ -21,7 +26,7 @@ test("create_cell", () => {
     grid.createCell(Settings.X_START, Settings.Y_START, Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
     for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
         for (let x = Settings.X_START; x < Settings.WIDTH; x++) {
-            if (x == Settings.X_START && y == Settings.Y_START) {
+            if (x === Settings.X_START && y === Settings.Y_START) {
                 expect(grid.cells[y][x]).toEqual(new Cell(Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED))
             } else {
                 expect(grid.cells[y][x]).toBe(null)
@@ -54,17 +59,118 @@ test("update_no_divide", () => {
     expect(count).toBe(1)
 })
 
-test("update_divide", () => {
-    grid.createCell(Settings.X_START, Settings.Y_START, Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
+test("update_divide_right", () => {
+    Math.random = () => 0
+    let x = Settings.X_START + 1
+    let y = Settings.Y_START + 1
+    grid.createCell(x, y, Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
     grid.update(Test.CELL_DIV_TIME_BASE)
     let count = 0
+    if (grid.cells[y][x + 1].time === Settings.CELL_START_TIME) {
+        count++
+    }
+    if (grid.cells[y][x].lifetime === Test.CELL_LIFETIME_BASE) {
+        count++
+    }
     for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
         for (let x = Settings.X_START; x < Settings.WIDTH; x++) {
-            if (grid.cells[y][x]==null) {
+            if (grid.cells[y][x] == null) {
                 count++
-            } else if (grid.cells[y][x].time == Settings.CELL_START_TIME) {
+            }
+        }
+    }
+    expect(count).toBe(Settings.HEIGHT * Settings.WIDTH)
+})
+
+test("update_divide_up", () => {
+    Math.random = () => 0.26
+    let x = Settings.X_START + 1
+    let y = Settings.Y_START + 1
+    grid.createCell(x, y, Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
+    grid.update(Test.CELL_DIV_TIME_BASE)
+    let count = 0
+    if (grid.cells[y - 1][x].time === Settings.CELL_START_TIME) {
+        count++
+    }
+    if (grid.cells[y][x].lifetime === Test.CELL_LIFETIME_BASE) {
+        count++
+    }
+    for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
+        for (let x = Settings.X_START; x < Settings.WIDTH; x++) {
+            if (grid.cells[y][x] == null) {
                 count++
-            } else if (grid.cells[y][x].time == Test.TIME_PASSED_BASE) {
+            }
+        }
+    }
+    expect(count).toBe(Settings.HEIGHT * Settings.WIDTH)
+})
+
+test("update_divide_left", () => {
+    Math.random = () => 0.51
+    let x = Settings.X_START + 1
+    let y = Settings.Y_START + 1
+    grid.createCell(x, y, Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
+    grid.update(Test.CELL_DIV_TIME_BASE)
+    let count = 0
+    if (grid.cells[y][x - 1].time === Settings.CELL_START_TIME) {
+        count++
+    }
+    if (grid.cells[y][x].lifetime === Test.CELL_LIFETIME_BASE) {
+        count++
+    }
+    for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
+        for (let x = Settings.X_START; x < Settings.WIDTH; x++) {
+            if (grid.cells[y][x] == null) {
+                count++
+            }
+        }
+    }
+    expect(count).toBe(Settings.HEIGHT * Settings.WIDTH)
+})
+
+test("update_divide_down", () => {
+    Math.random = () => 0.76
+    let x = Settings.X_START + 1
+    let y = Settings.Y_START + 1
+    grid.createCell(x, y, Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
+    grid.update(Test.CELL_DIV_TIME_BASE)
+    let count = 0
+    if (grid.cells[y + 1][x].time === Settings.CELL_START_TIME) {
+        count++
+    }
+    if (grid.cells[y][x].lifetime === Test.CELL_LIFETIME_BASE) {
+        count++
+    }
+    for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
+        for (let x = Settings.X_START; x < Settings.WIDTH; x++) {
+            if (grid.cells[y][x] == null) {
+                count++
+            }
+        }
+    }
+    expect(count).toBe(Settings.HEIGHT * Settings.WIDTH)
+})
+
+test("update_divide_some_disallowed", () => {
+    Math.random = () => 0
+    let x = Settings.X_START + 1
+    let y = Settings.Y_START + 1
+    grid.createCell(x, y, Test.CELL_LIFETIME_BASE, Test.CELL_DIV_TIME_BASE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
+    grid.createCell(x + 1, y, Test.CELL_LIFETIME_IMMORTAL, Test.CELL_DIV_TIME_CELIBATE, Test.CELL_DIV_FAIL_RATE_SUCCEED)
+    grid.update(Test.CELL_DIV_TIME_BASE)
+    let count = 0
+    if (grid.cells[y - 1][x].time === Settings.CELL_START_TIME) {
+        count++
+    }
+    if (grid.cells[y][x].lifetime === Test.CELL_LIFETIME_BASE) {
+        count++
+    }
+    if (grid.cells[y][x + 1].lifetime === Test.CELL_LIFETIME_IMMORTAL) {
+        count++
+    }
+    for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
+        for (let x = Settings.X_START; x < Settings.WIDTH; x++) {
+            if (grid.cells[y][x] == null) {
                 count++
             }
         }
--- src/components/cellGrowthSim/grid.tsx
+++ src/components/cellGrowthSim/grid.tsx
@@ -3,6 +3,7 @@ import { Settings, Directions } from './constants'
 
 class Grid {
     cells: (Cell | null)[][]
+    new_cells: { [key: string]: Cell }
     constructor() {
         this.cells = []
         for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
@@ -12,6 +13,23 @@ class Grid {
             }
             this.cells.push(row)
         }
+        this.new_cells = {}
+    }
+    display() {
+        for (let y = Settings.Y_START; y < Settings.HEIGHT; y++) {
+            for (let x = Settings.X_START; x < Settings.WIDTH; x++) {
+                if (this.cells[y][x]) {
+                    console.log(x, y, this.cells[y][x]!.time)
+                }
+            }
+        }
+    }
+    coordsToString(x: number, y: number) {
+        return (y * Settings.WIDTH + x).toString()
+    }
+    stringToCoords(str: string) {
+        let n = parseInt(str)
+        return [n % Settings.WIDTH, Math.floor(n / Settings.WIDTH)]
     }
     clear() {
         this.cells = this.cells.map(row => row.map(cell => null))
@@ -31,16 +49,16 @@ class Grid {
                         this.deleteCell(x, y)
                     } else if (status === Settings.CELL_DIVIDE_SIGNAL) {
                         let allowed = Directions.START
-                        if (x > Settings.X_START && !this.cells[y][x - Settings.CELL_GROWTH_DISTANCE]) {
+                        if (x > Settings.X_START && !this.cells[y][x - Settings.CELL_GROWTH_DISTANCE] && !(this.coordsToString(x - Settings.CELL_GROWTH_DISTANCE, y) in this.new_cells)) {
                             allowed += Directions.LEFT
                         }
-                        if (x < Settings.WIDTH - 1 && !this.cells[y][x + Settings.CELL_GROWTH_DISTANCE]) {
+                        if (x < Settings.WIDTH - 1 && !this.cells[y][x + Settings.CELL_GROWTH_DISTANCE] && !(this.coordsToString(x + Settings.CELL_GROWTH_DISTANCE, y) in this.new_cells)) {
                             allowed += Directions.RIGHT
                         }
-                        if (y > Settings.X_START && !this.cells[y - Settings.CELL_GROWTH_DISTANCE][x]) {
+                        if (y > Settings.X_START && !this.cells[y - Settings.CELL_GROWTH_DISTANCE][x] && !(this.coordsToString(x, y - Settings.CELL_GROWTH_DISTANCE) in this.new_cells)) {
                             allowed += Directions.UP
                         }
-                        if (y < Settings.HEIGHT - 1 && !this.cells[y + Settings.CELL_GROWTH_DISTANCE][x]) {
+                        if (y < Settings.HEIGHT - 1 && !this.cells[y + Settings.CELL_GROWTH_DISTANCE][x] && !(this.coordsToString(x, y + Settings.CELL_GROWTH_DISTANCE) in this.new_cells)) {
                             allowed += Directions.DOWN
                         }
                         let spot = Directions.START
@@ -57,11 +75,16 @@ class Grid {
                             }
                             let directionX = Math.abs(direction - Directions.HORI_FUNC_POINT_X) + Directions.HORI_FUNC_POINT_Y
                             let directionY = Math.abs(direction - Directions.VERT_FUNC_POINT_X) + Directions.VERT_FUNC_POINT_Y
-                            this.createCell(x + directionX, y + directionY, this.cells[y][x]!.lifetime, this.cells[y][x]!.divTime, this.cells[y][x]!.divFailRate)
+                            this.new_cells[this.coordsToString(x + directionX, y + directionY)] = new Cell(this.cells[y][x]!.lifetime, this.cells[y][x]!.divTime, this.cells[y][x]!.divFailRate)
                         }
                     }
                 }
             }
+        }
+        for (let key in this.new_cells) {
+            let [x, y] = this.stringToCoords(key)
+            this.cells[y][x] = this.new_cells[key]
+            delete this.new_cells[key]
         }
     }
 }
