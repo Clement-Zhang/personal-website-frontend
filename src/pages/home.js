6bb5ac94be2686b9ecd268893a9841dcda1bbad5
Clement-Zhang <zhangclement947@yahoo.ca>
2024-11-28 17:26:27 -0500

    adjustment to home screen

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ const Home = () => {
             <p>
                 Hello there! I'm Clement Zhang, a Computer Science new graduate from Toronto Metropolitan University 
                 (formerly Ryerson University). I'm interested in software engineering and web development. 
-                I'm currently looking for an internship. I made this website to showcase my skills in 
+                I'm currently looking for a job. I made this website to showcase my skills in 
                 React. In the future, I will improve the pages and add more content.
             </p>
             <p>
