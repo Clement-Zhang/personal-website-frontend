7f210477f0666e5aa94070c8e7c8e9f0fdf91e90
Clement-Zhang <zhangclement947@yahoo.ca>
2025-05-30 21:57:36 -0400

    chat ui

--- src/pages/ama.js
+++ src/pages/ama.js
@@ -1,6 +1,14 @@
 import Default from "../templates/default"
-import styles from "../styles/ama.module.css"
+import styles from "../styles/ama.module.css";
+import library_styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { ChatContainer, MessageList, Message } from '@chatscope/chat-ui-kit-react';
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 const Ama = () => {
     const [prompt, changePrompt] = useState("");
     const [chatlog, updateChatlog] = useState([]);
@@ -13,9 +21,8 @@ const Ama = () => {
         const result = await res.json();
         updateChatlog((chatlog) => [
             ...chatlog,
-            result.response
+            [prompt, result.response]
         ]);
-        console.log("chatlog update", chatlog);
     }
     return (
         <Default>
@@ -23,21 +30,61 @@ const Ama = () => {
                 personally answer everyone's questions. Or, if you will, this is a glorified
                 compressed automated FAQ.
             </p>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    console.log(submitPrompt());
-                    changePrompt("");
-                }}>
-                <input
-                    type="text"
-                    value={prompt}
-                    onChange={(e) => changePrompt(e.target.value)}
-                />
-            </form>
-            {chatlog.map((msg) => (
-                <p>{msg}</p>
-            ))}
+            <div className={styles.output}>
+                <ChatContainer>
+                    <MessageList>
+                        {chatlog.flatMap(([prompt, response], i) => [
+                            <Message
+                                key={i * 2}
+                                model={{
+                                    message: prompt,
+                                    sender: "User",
+                                    direction: "outgoing",
+                                }}
+                            />,
+                            <Message
+                                key={i * 2 + 1}
+                                model={{
+                                    message: response,
+                                    sender: "Chatbot",
+                                    direction: "incoming",
+                                }}
+                            />
+                        ])}
+                    </MessageList>
+                </ChatContainer>
+            </div>
+            <div className={styles.input}>
+                <Form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        submitPrompt();
+                        changePrompt("");
+                    }}>
+                    <Row>
+                        <Col>
+                            <div className={styles.box}>
+                                <Form.Control
+                                    className={styles.text}
+                                    as="textarea"
+                                    value={prompt}
+                                    onChange={(e) => {
+                                        changePrompt(e.target.value);
+                                        let elem = e.target;
+                                        elem.style.height = "auto";
+                                        elem.style.height = elem.scrollHeight + "px";
+                                    }}
+                                />
+                            </div>
+                        </Col>
+                        <Col>
+                            <Button variant="primary" type="submit">
+                                <FontAwesomeIcon icon={faPaperPlane} />
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </div>
         </Default>
     );
 }
