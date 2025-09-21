4fd231e948c655883cf216826e494c2fa841c807
Clement-Zhang <zhangclement947@yahoo.ca>
2025-09-21 17:22:50 -0400

    fix 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: { comic: ['"Comic Sans MS"', ...fontFamily.sans] },
+            colors: {
+                chatbot: {
+                    message: '#35363A',
+                },
+            },
         },
     },
     plugins: [],
