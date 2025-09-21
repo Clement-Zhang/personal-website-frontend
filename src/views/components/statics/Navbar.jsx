9f1d9bd647ebf65617c56986ac2461f0cee4fe4d
Clement-Zhang <zhangclement947@yahoo.ca>
2025-09-21 17:04:51 -0400

    stop updating navbar

--- src/views/components/statics/Navbar.jsx
+++ src/views/components/statics/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 flex items-center justify-center gap-4 *:text-xl">
+        <nav className="flex items-center justify-center gap-4 *:text-xl">
             <Link to="/" className="!text-3xl">
                 About Me
             </Link>
