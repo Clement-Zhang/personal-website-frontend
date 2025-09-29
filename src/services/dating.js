95bd4fdb19c4249564db51d163b992155f90285f
Clement-Zhang <zhangclement947@yahoo.ca>
2025-09-28 21:56:54 -0400

    done

--- src/services/dating.js
+++ src/services/dating.js
@@ -1,17 +1,16 @@
 import socket from '../globals/socket';
 
 async function submit(text) {
-    const response = socket.emit('reformat', text, (_, response) => response);
-    console.log(response);
+    const response = await socket.emitWithAck('reformat', text);
     if (response.includes('fail')) {
         return 'You need to provide more information for me to generate matches. Start with your gender and sexuality. Providing likes and dislikes will allow me to generate better matches.';
     } else {
-        const match = socket.emit('match', response, (_, response) => response);
+        const match = await socket.emitWithAck('match', response);
         return JSON.stringify(match, null, '\t');
     }
 }
 async function reset() {
-    await fetch(process.env.REACT_APP_BACKEND + '/reset', {
+    await fetch(process.env.REACT_APP_BACKEND_HTTP + '/reset', {
         method: 'POST',
     });
 }
