57237e53604b2214f116ff7fb6d65bc04ef6fa80
Clement-Zhang <zhangclement947@yahoo.ca>
2026-03-12 22:15:11 -0400

    Update Navbar.jsx

--- src/views/components/statics/Navbar.jsx
+++ src/views/components/statics/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     return (
         <nav className="flex sticky top-0 items-center justify-center gap-4 *:text-xl m-2 bg-dark h-12">
             {tabs.map((tab) => (
-                <Link to={tab.path} key={tab.path} className={style}>
+                <Link to={tab.path} key={tab.path} className={tab.style}>
                     {tab.label}
                 </Link>
             ))}
