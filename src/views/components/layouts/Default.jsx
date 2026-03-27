eccca49f4648ef8aef1c529edfe13a533f0c7945
Clement-Zhang <zhangclement947@yahoo.ca>
2026-03-26 21:37:19 -0400

    Update Default.jsx

--- src/views/components/layouts/Default.jsx
+++ src/views/components/layouts/Default.jsx
@@ -5,7 +5,7 @@ export default function Default() {
     return (
         <div className="font-comic">
             <Navbar />
-            <div className={'max-w-sm lg:max-w-4xl mx-auto my-2'}>
+            <div className={'max-w-lg lg:max-w-4xl mx-auto my-2'}>
                 <Outlet />
             </div>
         </div>
