11d2044d25830041686a4f0237d642901072512a
Clement-Zhang <zhangclement947@yahoo.ca>
2026-03-26 20:02:02 -0400

    Update Navbar.jsx

--- src/views/components/statics/Navbar.jsx
+++ src/views/components/statics/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         { label: 'Dating App POC', path: '/dating' },
     ];
     return (
-        <nav className="flex sticky top-0 justify-center gap-4 *:text-xl bg-zinc-700 h-12">
+        <nav className="flex sticky top-0 justify-center gap-4 *:text-xl bg-zinc-900 h-12">
             {tabs.map((tab) => (
                 <Link
                     to={tab.path}
