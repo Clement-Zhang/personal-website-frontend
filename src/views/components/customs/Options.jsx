27d147b6f977762a5045a1f3efcaf5fec7074839
Clement-Zhang <zhangclement947@yahoo.ca>
2026-06-06 19:29:31 -0400

    sliding options

--- src/views/components/customs/Options.jsx
+++ src/views/components/customs/Options.jsx
@@ -1,5 +1,6 @@
 import icon from '../../../assets/images/calculators/hackers/options.jpg';
 import { createContext, useContext, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const OptionsContext = createContext();
 
@@ -16,14 +17,21 @@ export const OptionsProvider = ({ children }) => {
     return (
         <OptionsContext.Provider value={{ setOptions, toggleOptions }}>
             <div className="m-4">{children}</div>
-            <div
-                className={
-                    'absolute top-0 w-full h-full bg-black rounded-3xl p-4 z-49 ' +
-                    (!open && 'hidden')
-                }
-            >
-                {Options}
-            </div>
+            <AnimatePresence>
+                {open && (
+                    <motion.div
+                        initial={{ x: '100vw' }}
+                        animate={{ x: 0 }}
+                        transition={{ duration: 0.5 }}
+                        className={
+                            'absolute top-0 w-full h-full bg-black rounded-3xl p-4 z-49 ' +
+                            (!open && 'hidden')
+                        }
+                    >
+                        {Options}
+                    </motion.div>
+                )}
+            </AnimatePresence>
             <img
                 src={icon}
                 alt="options"
