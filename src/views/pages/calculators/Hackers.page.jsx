eed1273cce3a2a2db2d11accfe5249aa59a87615
Clement-Zhang <zhangclement947@yahoo.ca>
2026-06-06 20:08:59 -0400

    add program tree

--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -1,3 +1,16 @@
+import { useOptions } from '../../components/customs/Options';
+import program_tree from '../../../assets/images/calculators/hackers/program tree.jpg';
+import { useEffect } from 'react';
+
 export default function Hackers() {
+    const { toggleOptions, setOptions } = useOptions();
+    useEffect(() => {
+        setOptions(
+            <div className="flex flex-col">
+                <h1 className="text-2xl mb-4 justify-center">Select Program Levels</h1>
+                <img src={program_tree} alt="program tree"></img>
+            </div>,
+        );
+    });
     return <h1 className="text-8xl">Hackers</h1>;
 }
