1ad616061240818d611231750f41cd6cf26a394d
Clement-Zhang <zhangclement947@yahoo.ca>
2026-06-06 20:22:35 -0400

    Update Hackers.page.jsx

--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -6,9 +6,17 @@ export default function Hackers() {
     const { toggleOptions, setOptions } = useOptions();
     useEffect(() => {
         setOptions(
-            <div className="flex flex-col">
-                <h1 className="text-2xl mb-4 justify-center">Select Program Levels</h1>
-                <img src={program_tree} alt="program tree"></img>
+            <div className="flex">
+                <div>
+                    <h1 className="text-2xl mb-4 justify-center">
+                        Select Program Levels
+                    </h1>
+                    <img
+                        src={program_tree}
+                        className="w-1/2 h-1/2"
+                        alt="program tree"
+                    ></img>
+                </div>
             </div>,
         );
     });
