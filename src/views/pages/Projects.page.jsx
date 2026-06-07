b392c3f95cfb54da8d627410306513a6f10589e4
Clement-Zhang <zhangclement947@yahoo.ca>
2026-06-06 20:30:48 -0400

    Update Projects.page.jsx

--- src/views/pages/Projects.page.jsx
+++ src/views/pages/Projects.page.jsx
@@ -37,7 +37,7 @@ export default function Projects() {
                         <img
                             src={project.img}
                             alt={project.alt}
-                            className="block w-1/2 h-1/2"
+                            className="block scale-50"
                         />
                         <p>{project.description}</p>
                     </div>
