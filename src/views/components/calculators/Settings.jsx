b13dff7783f7aff5013f18e14ba777a01723704f
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-13 19:47:47 -0400

    hover text positioning

--- src/views/components/calculators/Settings.jsx
+++ src/views/components/calculators/Settings.jsx
@@ -1,29 +1,120 @@
 import icon from '../../../assets/images/calculators/hackers/explain.jpg';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 export default function Settings({ data, onChange }) {
-    const [hover, setHover] = useState(null);
+    const [tooltip, setTooltip] = useState(null);
+    const [tooltipPosition, setTooltipPosition] = useState({});
+    const tooltipRef = useRef(null);
+    useEffect(() => {
+        if (tooltip) {
+            const rect = tooltipRef.current.getBoundingClientRect();
+            let edge = '';
+            let align = '';
+            let center = [
+                tooltip.activeIcon.left + tooltip.activeIcon.width / 2,
+                tooltip.activeIcon.top + tooltip.activeIcon.height / 2,
+            ];
+            if (tooltip.activeIcon.top - rect.height < 0) {
+                edge = 'top';
+            } else if (
+                tooltip.activeIcon.bottom + rect.height >
+                window.innerHeight
+            ) {
+                edge = 'bottom';
+            } else if (tooltip.activeIcon.left - rect.width < 0) {
+                edge = 'left';
+            } else {
+                edge = 'right';
+            }
+            if (edge == 'top' || edge == 'bottom') {
+                if (
+                    center[0] - rect.width / 2 > 0 &&
+                    center[0] + rect.width / 2 < window.innerWidth
+                ) {
+                    align = 'middle';
+                } else if (
+                    tooltip.activeIcon.left + rect.width <
+                    window.innerWidth
+                ) {
+                    align = 'left';
+                } else {
+                    align = 'right';
+                }
+            } else {
+                if (
+                    center[1] - rect.height / 2 > 0 &&
+                    center[1] + rect.height / 2 < window.innerHeight
+                ) {
+                    align = 'middle';
+                } else if (
+                    tooltip.activeIcon.top + rect.height <
+                    window.innerHeight
+                ) {
+                    align = 'top';
+                } else {
+                    align = 'bottom';
+                }
+            }
+            let position = {};
+            switch (edge) {
+                case 'top':
+                    position.bottom = tooltip.activeIcon.top;
+                    break;
+                case 'bottom':
+                    position.top = tooltip.activeIcon.bottom;
+                    break;
+                case 'left':
+                    position.right = tooltip.activeIcon.left;
+                    break;
+                case 'right':
+                    position.left = tooltip.activeIcon.right;
+                    break;
+            }
+            switch (align) {
+                case 'left':
+                    position.left = tooltip.activeIcon.left;
+                    break;
+                case 'right':
+                    position.right = tooltip.activeIcon.right;
+                    break;
+                case 'middle':
+                    edge == 'top' || edge == 'bottom'
+                        ? (position.left = center[0] - rect.width / 2)
+                        : (position.top = center[1] - rect.height / 2);
+                    break;
+            }
+            setTooltipPosition(position);
+        }
+    }, [tooltip]);
+    let style = {};
     return (
         <form className="flex gap-1">
+            {tooltip && (
+                <p
+                    ref={tooltipRef}
+                    className="fixed bg-text-background text-zinc-900 rounded-md max-w-lg"
+                    style={tooltipPosition}
+                >
+                    {tooltip.text}
+                </p>
+            )}
             {data.map((input) => {
                 return (
                     <div key={input.name} className="flex gap-1">
                         <label htmlFor={input.name} className="flex gap-1">
                             <p>{input.name}</p>
-                            <div className="relative">
-                                <img
-                                    src={icon}
-                                    alt="explain"
-                                    onMouseEnter={() => setHover(input.name)}
-                                    onMouseLeave={() => setHover(null)}
-                                    className=""
-                                ></img>
-                                {hover == input.name && (
-                                    <p className="absolute bottom-full left-1/2 -translate-x-1/2 bg-text-background text-zinc-900 rounded-md max-w-">
-                                        {input.explain}
-                                    </p>
-                                )}
-                            </div>
+                            <img
+                                src={icon}
+                                alt="explain"
+                                onMouseEnter={(e) =>
+                                    setTooltip({
+                                        text: input.explain,
+                                        activeIcon:
+                                            e.target.getBoundingClientRect(),
+                                    })
+                                }
+                                onMouseLeave={() => setTooltip(null)}
+                            ></img>
                         </label>
                         <input
                             value={input.value}
