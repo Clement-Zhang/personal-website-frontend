4b3196d7507e70ece537ec417e066ad4de3bb427
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-14 13:18:24 -0400

    Update Tooltip.jsx

--- src/views/components/calculators/Tooltip.jsx
+++ src/views/components/calculators/Tooltip.jsx
@@ -83,7 +83,7 @@ export default function Tooltip({ spec }) {
             {spec && (
                 <p
                     ref={tooltipRef}
-                    className="fixed bg-text-background text-zinc-900 rounded-md max-w-lg"
+                    className="fixed bg-text-background text-zinc-900 rounded-md max-w-3xs"
                     style={tooltipPosition}
                 >
                     {spec.text}
