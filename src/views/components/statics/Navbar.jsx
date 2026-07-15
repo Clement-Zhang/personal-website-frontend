fed5b88e08546ddd3dd1ff6b56141dfe7a579403
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-14 20:46:16 -0400

    Update Navbar.jsx

--- src/views/components/statics/Navbar.jsx
+++ src/views/components/statics/Navbar.jsx
@@ -2,18 +2,18 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     const tabs = [
-        { label: 'Clement Zhang', path: '/', style: 'text-3xl!' },
+        { label: 'Clement Zhang', path: '/', style: '!text-3xl' },
         { label: 'My Projects', path: '/projects' },
         { label: 'Dating App POC', path: '/dating' },
     ];
     return (
-        <nav className="flex sticky top-0 justify-center *:text-xl bg-black h-20 sm:h-12">
+        <nav className="flex sticky top-0 justify-center *:text-xl bg-black h-12">
             {tabs.map((tab) => (
                 <Link
                     to={tab.path}
                     key={tab.path}
                     className={
-                        'flex items-center hover:bg-zinc-700 h-full px-4 text-inherit no-underline text-center' +
+                        'flex items-center hover:bg-zinc-700 h-full px-4 text-inherit no-underline ' +
                         tab.style
                     }
                 >
