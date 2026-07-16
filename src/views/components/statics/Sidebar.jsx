a914b3a622e0b2693c1f58b6a45a880ffd460162
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-16 17:12:42 -0400

    Update Sidebar.jsx

--- src/views/components/statics/Sidebar.jsx
+++ src/views/components/statics/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
     return (
-        <nav className="flex flex-col sticky top-0 bg-gray-700 h-screen max-w-64">
+        <nav className="flex flex-col sticky top-0 bg-gray-700 min-h-screen max-w-64">
             {tabs.map((tab) => (
                 <Link to={tab.path} key={tab.path}>
                     <img src={tab.icon}></img>
