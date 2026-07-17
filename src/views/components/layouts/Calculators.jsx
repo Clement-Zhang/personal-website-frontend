eb1e706788eef911333dcf9d86b597b601bef7eb
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-17 14:00:36 -0400

    Update Calculators.jsx

--- src/views/components/layouts/Calculators.jsx
+++ src/views/components/layouts/Calculators.jsx
@@ -4,7 +4,9 @@ import { Outlet } from 'react-router';
 export default function Calculators() {
     return (
         <div className="flex">
-            <Sidebar />
+            <div className="shrink-0">
+                <Sidebar />
+            </div>
             <div className="flex-1 relative">
                 <Outlet />
             </div>
