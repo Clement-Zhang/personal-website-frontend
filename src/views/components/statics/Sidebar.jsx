b7d24fe00d04e71c8ba5f1bb5c90741316399b78
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-17 13:56:21 -0400

    try new sidebar

--- src/views/components/statics/Sidebar.jsx
+++ src/views/components/statics/Sidebar.jsx
@@ -1,17 +1,41 @@
 import tabs from '@/configs/tabs/calculators.config';
+import icon from '@/assets/images/calculators/options_s.jpg';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Sidebar() {
+    const [open, setOpen] = useState(false);
+    function toggleSidebar() {
+        setOpen((prev) => !prev);
+    }
     return (
         <>
-            <div className="md:hidden">
-                <nav className="flex flex-col top-0 bg-gray-700 md:w-64 min-h-screen">
-                    {tabs.map((tab) => (
-                        <Link to={tab.path} key={tab.path}>
-                            <img src={tab.icon}></img>
-                        </Link>
-                    ))}
-                </nav>
+            <div className="md:hidden flex">
+                <AnimatePresence>
+                    {open && (
+                        <motion.div
+                            initial={{ x: '-100%' }}
+                            exit={{ x: '-100%' }}
+                            animate={{ x: 0 }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            <nav className="flex flex-col top-0 bg-gray-700 w-64 min-h-screen">
+                                {tabs.map((tab) => (
+                                    <Link to={tab.path} key={tab.path}>
+                                        <img src={tab.icon}></img>
+                                    </Link>
+                                ))}
+                            </nav>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+                <img
+                    src={icon}
+                    alt="sidebar"
+                    className="z-50"
+                    onClick={toggleSidebar}
+                ></img>
             </div>
             <nav className="hidden md:flex flex-col top-0 bg-gray-700 w-64 min-h-screen">
                 {tabs.map((tab) => (
