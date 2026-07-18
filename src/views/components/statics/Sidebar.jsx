3b5948359771e1fe64ae082d5b53adc6a4985448
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-17 20:11:23 -0400

    cooler sidebar

--- src/views/components/statics/Sidebar.jsx
+++ src/views/components/statics/Sidebar.jsx
@@ -5,40 +5,34 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Sidebar() {
-    const [extend, setExtend] = useState(false);
+    const [extend, setExtend] = useState(window.innerWidth >= 768);
     function toggleExtend() {
         setExtend((prev) => !prev);
     }
     return (
-        <>
-            <div className="md:hidden">
-                <nav className="flex flex-col top-0 bg-gray-700 w-12 min-h-screen items-center">
-                    <img
-                        src={icon}
-                        alt="extend sidebar"
-                        className="relative w-6 h-6 m-2 cursor-pointer"
-                        onClick={toggleExtend}
-                    ></img>
-                    {tabs.map((tab) => (
-                        <Link to={tab.path} key={tab.path}>
-                            <img src={tab.iconAlt}></img>
-                        </Link>
-                    ))}
-                </nav>
-                {/* <AnimatePresence>
-                    {extend && (
-                        <motion.div transition={{ duration: 0.3 }}>
-                        </motion.div>
-                    )}
-                </AnimatePresence> */}
-            </div>
-            <nav className="hidden md:flex flex-col top-0 bg-gray-700 w-64 min-h-screen">
-                {tabs.map((tab) => (
-                    <Link to={tab.path} key={tab.path}>
-                        <img src={tab.icon}></img>
-                    </Link>
-                ))}
-            </nav>
-        </>
+        <AnimatePresence>
+            {extend && (
+                <motion.div
+                    animate={{ width: extend ? 256 : 48 }}
+                    transition={{ duration: 0.3 }}
+                >
+                    <nav className="flex flex-col top-0 bg-gray-700 w-12 md:w-64 min-h-screen items-center">
+                        <img
+                            src={icon}
+                            alt="extend sidebar"
+                            className="relative w-6 h-6 m-2 cursor-pointer md:hidden"
+                            onClick={toggleExtend}
+                        ></img>
+                        {tabs.map((tab) => (
+                            <Link to={tab.path} key={tab.path}>
+                                <img
+                                    src={extend ? tab.icon : tab.iconAlt}
+                                ></img>
+                            </Link>
+                        ))}
+                    </nav>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 }
