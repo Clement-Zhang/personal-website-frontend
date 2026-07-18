47db38fd283403f3b685ee9e37248b1308f6a6cc
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-18 16:49:20 -0400

    input boxes

--- src/views/components/calculators/Characters.jsx
+++ src/views/components/calculators/Characters.jsx
@@ -6,6 +6,25 @@ export default function Characters({ img, characters, inputs, onChange }) {
                 className="w-full flex justify-center"
                 alt={img.alt}
             ></img>
+            {Object.keys(inputs).map((program) => (
+                <input
+                    value={characters[program]}
+                    type="number"
+                    step="1"
+                    min="0"
+                    onChange={(e) =>
+                        onChange({
+                            name: input.name,
+                            value: e.target.value,
+                        })
+                    }
+                    style={{
+                        left: inputs[program][0]+"%",
+                        top: inputs[program][1]+"%",
+                    }}
+                    className="absolute"
+                ></input>
+            ))}
         </div>
     );
 }
