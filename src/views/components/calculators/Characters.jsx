2a44147a55c589d7f12e675dcfc2889adb7e9839
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-18 23:54:14 -0400

    Update Characters.jsx

--- src/views/components/calculators/Characters.jsx
+++ src/views/components/calculators/Characters.jsx
@@ -3,20 +3,26 @@ export default function Characters({ img, characters, inputs, onChange }) {
         <div className="relative">
             <img src={img.src} className="w-full" alt={img.alt} />
             {Object.keys(inputs).map((program) => (
-                <input
-                    key={program}
-                    value={characters[program]}
-                    type="number"
-                    step="1"
-                    min="0"
-                    onChange={(e) =>
-                        onChange({
-                            name: program,
-                            value: e.target.value,
-                        })
-                    }
+                // <input
+                //     key={program}
+                //     value={characters[program]}
+                //     type="number"
+                //     step="1"
+                //     min="0"
+                //     onChange={(e) =>
+                //         onChange({
+                //             name: program,
+                //             value: e.target.value,
+                //         })
+                //     }
+                //     className={
+                //         'absolute w-6 outline-hidden text-right ' +
+                //         inputs[program]
+                //     }
+                // />
+                <div
                     className={
-                        'absolute w-6 outline-hidden text-right ' +
+                        'absolute h-2 w-2 rounded-full bg-red-500 ' +
                         inputs[program]
                     }
                 />
