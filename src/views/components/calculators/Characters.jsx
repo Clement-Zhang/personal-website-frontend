ca78a8b9ce8751300114039894a1a860790ea2ad
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-19 00:02:51 -0400

    Update Characters.jsx

--- src/views/components/calculators/Characters.jsx
+++ src/views/components/calculators/Characters.jsx
@@ -3,29 +3,31 @@ export default function Characters({ img, characters, inputs, onChange }) {
         <div className="relative">
             <img src={img.src} className="w-full" alt={img.alt} />
             {Object.keys(inputs).map((program) => (
-                // <input
-                //     key={program}
-                //     value={characters[program]}
-                //     type="number"
-                //     step="1"
-                //     min="0"
-                //     onChange={(e) =>
-                //         onChange({
-                //             name: program,
-                //             value: e.target.value,
-                //         })
-                //     }
-                //     className={
-                //         'absolute w-6 outline-hidden text-right ' +
-                //         inputs[program]
-                //     }
-                // />
-                <div
-                    className={
-                        'absolute h-2 w-2 rounded-full bg-red-500 ' +
-                        inputs[program]
-                    }
-                />
+                <>
+                    <input
+                        key={program}
+                        value={characters[program]}
+                        type="number"
+                        step="1"
+                        min="0"
+                        onChange={(e) =>
+                            onChange({
+                                name: program,
+                                value: e.target.value,
+                            })
+                        }
+                        className={
+                            'absolute w-6 outline-hidden border-red-500 text-right ' +
+                            inputs[program]
+                        }
+                    />
+                    <div
+                        className={
+                            'absolute h-2 w-2 rounded-full bg-red-500 ' +
+                            inputs[program]
+                        }
+                    />
+                </>
             ))}
         </div>
     );
