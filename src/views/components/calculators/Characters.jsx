6972c95e6c9d6f87710d22244364d75fb01f85a8
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-20 14:01:49 -0400

    Update Characters.jsx

--- src/views/components/calculators/Characters.jsx
+++ src/views/components/calculators/Characters.jsx
@@ -17,7 +17,7 @@ export default function Characters({ img, characters, inputs, onChange }) {
                             })
                         }
                         className={
-                            'absolute outline-hidden border text-right xl:text-base text-xs ' +
+                            'absolute outline-hidden border text-righttext-sm!  xl:text-base! ' +
                             inputs[program]
                         }
                     />
