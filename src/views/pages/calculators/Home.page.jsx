15c80ba8d072f11e3d05de0766b3fcbb9ca9aefc
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-21 16:24:03 -0400

    Update Home.page.jsx

--- src/views/pages/calculators/Home.page.jsx
+++ src/views/pages/calculators/Home.page.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1 className="text-5xl! sm:text-6xl! md:text-5xl! lg:text-7xl! xl:text-8xl! flex h-screen items-center justify-center">&larr;Choose a calculator</h1>;
+    return <h1 className="text-3xl! sm:text-6xl! md:text-5xl! lg:text-7xl! xl:text-8xl! flex h-screen items-center justify-center">&larr;Choose a calculator</h1>;
 }
