c7c4b2e721415180af6f4cf87f3b84a29e39dc2e
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-21 14:04:57 -0400

    make snapping work for negative values

--- src/views/components/calculators/Settings.jsx
+++ src/views/components/calculators/Settings.jsx
@@ -47,6 +47,7 @@ export default function Settings({ settings, onChange }) {
                                     ) *
                                         element.step +
                                         min,
+                                    0,
                                 ),
                             });
                         }}
