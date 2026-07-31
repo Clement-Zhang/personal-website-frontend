d39c5f48a608ab5b6bb0e0d033940e956dc5caa9
Clement-Zhang <zhangclement947@yahoo.ca>
2026-07-31 00:22:43 -0400

    Update Hackers.page.jsx

--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -1,5 +1,5 @@
 import { getSettings, getPrograms } from '@/configs/calculators/hackers.config';
-import { nodes } from '@/configs/calculators/hackers.config';
+import { nodes } from '@/data/calculators/hackers';
 import program_tree from '@/assets/images/calculators/hackers/program tree.jpg';
 import { useOptions } from '../../components/customs/Options';
 import Section from '../../components/customs/Section';
