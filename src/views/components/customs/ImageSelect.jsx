dd30b619a91987500b31bb90ccbb6f6ed1f44d73
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-02 15:50:08 -0400

    Update ImageSelect.jsx

--- src/views/components/customs/ImageSelect.jsx
+++ src/views/components/customs/ImageSelect.jsx
@@ -10,7 +10,7 @@ export default function ImageSelect({ value, onChange, options }) {
     return (
         <div className="relative" onBlur={() => setOpen(false)}>
             <div
-                className="flex items-center border-y border-s cursor-pointer"
+                className="flex items-center border-y border-e cursor-pointer"
                 onClick={() => setOpen((prev) => !prev)}
             >
                 <img
