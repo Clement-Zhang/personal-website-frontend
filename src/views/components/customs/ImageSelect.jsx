6bc21c674f5b6ec10fb8ab3947fd43b4a28a6ac3
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-01 23:52:01 -0400

    Update ImageSelect.jsx

--- src/views/components/customs/ImageSelect.jsx
+++ src/views/components/customs/ImageSelect.jsx
@@ -18,7 +18,7 @@ export default function ImageSelect({ value, onChange, options }) {
                     alt={selected.value}
                     className="w-12"
                 />
-                <img src={arrow} alt="down arrow" className="w-2 h-2" />
+                <img src={arrow} alt="down arrow" className="size-3" />
             </div>
 
             <AnimatePresence>
