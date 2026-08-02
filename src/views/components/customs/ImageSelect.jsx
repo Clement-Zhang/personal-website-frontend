adac1767f26cf155d6275c91177f704d63aaee66
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-02 16:56:26 -0400

    Update ImageSelect.jsx

--- src/views/components/customs/ImageSelect.jsx
+++ src/views/components/customs/ImageSelect.jsx
@@ -27,7 +27,7 @@ export default function ImageSelect({ value, onChange, options }) {
                         initial={{ scaleY: 0 }}
                         animate={{ scaleY: 1 }}
                         exit={{ scaleY: 0 }}
-                        className="absolute top-full list-none ps-0 origin-top **:my-px *:w-12"
+                        className="absolute top-full list-none ps-0 origin-top **:my-px **:w-12"
                     >
                         {options.map((option) => (
                             <li
