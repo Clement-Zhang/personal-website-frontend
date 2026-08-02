0ee97ee8205781afb28ed653357d8b42b2056979
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-01 23:33:02 -0400

    make sure prep didn't break anything

--- src/views/components/customs/ImageSelect.jsx
+++ src/views/components/customs/ImageSelect.jsx
@@ -7,11 +7,11 @@ export default function ImageSelect({ value, onChange, options }) {
         options.find((option) => option.value == value),
     );
     return (
-        <div className="relative **:w-12" onBlur={() => setOpen(false)}>
+        <div className="relative" onBlur={() => setOpen(false)}>
             <img
                 src={selected.image}
                 alt={selected.value}
-                className="cursor-pointer border"
+                className="cursor-pointer border w-12"
                 onClick={() => setOpen((prev) => !prev)}
             />
             <AnimatePresence>
@@ -20,7 +20,7 @@ export default function ImageSelect({ value, onChange, options }) {
                         initial={{ scaleY: 0 }}
                         animate={{ scaleY: 1 }}
                         exit={{ scaleY: 0 }}
-                        className="absolute top-full list-none ps-0 origin-top"
+                        className="absolute top-full list-none ps-0 origin-top **:w-12"
                     >
                         {options.map((option) => (
                             <li
