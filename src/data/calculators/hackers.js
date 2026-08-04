5e350ab24f34927345ce16f7005cbbfd52b51322
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-04 16:40:06 -0400

    add node levels dropdown

--- src/data/calculators/hackers.js
+++ src/data/calculators/hackers.js
@@ -7,3 +7,16 @@ export const nodes = Object.entries(
     acc.push({ image: url, value: path.split('/').at(-2) });
     return acc;
 }, []);
+
+export const attackerLevels = Object.entries(
+    import.meta.glob(
+        [
+            '../../assets/images/calculators/hackers/gameImgs/nodes/*/*.jpg',
+            '!../../assets/images/calculators/hackers/gameImgs/nodes/*/face.jpg',
+        ],
+        { eager: true, import: 'default' },
+    ),
+).reduce((acc, [path, url]) => {
+    acc.push({ image: url, value: path.split('/').at(-1) });
+    return acc;
+}, []);
--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -1,5 +1,6 @@
 import { getSettings, getPrograms } from '@/configs/calculators/hackers.config';
 import { nodes } from '@/data/calculators/hackers';
+import { attackerLevels } from '../../../data/calculators/hackers';
 import program_tree from '@/assets/images/calculators/hackers/program tree.jpg';
 import { useOptions } from '../../components/customs/Options';
 import Section from '../../components/customs/Section';
@@ -12,6 +13,7 @@ export default function Hackers() {
     const [settingsData, setSettingsData] = useState(getSettings);
     const [programsData, setProgramsData] = useState(getPrograms);
     const [attacker, setAttacker] = useState(nodes[0].value);
+    const [attackerLevel, setAttackerLevel] = useState(attackerLevels[0].value);
     const { setOptions } = useOptions();
     useEffect(() => {
         setOptions(
@@ -51,6 +53,17 @@ export default function Hackers() {
                         <ImageSelect
                             value={attacker}
                             onChange={setAttacker}
+                            options={nodes}
+                        />
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <p>Node Level</p>
+                        <ImageSelect
+                            value={attackerLevel}
+                            onChange={(level) => {
+                                setAttacker(level);
+                                console.log(level);
+                            }}
                             options={nodes}
                         />
                     </div>
