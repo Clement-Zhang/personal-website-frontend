7231f4c048578f549671c16e207b72b4f9daa8ed
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-06 17:48:23 -0400

    change data architecture

--- src/data/calculators/hackers.js
+++ src/data/calculators/hackers.js
@@ -1,25 +1,32 @@
-export const nodes = Object.entries(
+const allNodes = Object.entries(
     import.meta.glob(
-        '../../assets/images/calculators/hackers/gameImgs/nodes/*/face.jpg',
+        '../../assets/images/calculators/hackers/gameImgs/nodes/*/*.jpg',
         { eager: true, import: 'default' },
     ),
 ).reduce((acc, [path, url]) => {
-    acc.push({ image: url, value: path.split('/').at(-2) });
+    const node = path.split('/').at(-2);
+    acc[node]
+        ? acc[node].push({
+              image: url,
+              value: path.split('/').at(-1).slice(0, -4).split('-'),
+          })
+        : (acc[node] = [
+              {
+                  image: url,
+                  value: path.split('/').at(-1).slice(0, -4).split('-'),
+              },
+          ]);
     return acc;
-}, []);
+}, {});
 
-export const attackerLevels = Object.entries(
-    import.meta.glob(
-        [
-            '../../assets/images/calculators/hackers/gameImgs/nodes/*/*.jpg',
-            '!../../assets/images/calculators/hackers/gameImgs/nodes/*/face.jpg',
-        ],
-        { eager: true, import: 'default' },
-    ),
-).reduce((acc, [path, url]) => {
-    acc.push({
-        image: url,
-        value: path.split('/').at(-1).slice(0, -4).split('-').at(0),
-    });
-    return acc;
-}, []).sort((before, after)=>before.value - after.value)
+for (let node in allNodes) {
+    allNodes[node].sort((before, after) => before.value[1] - after.value[0]);
+}
+
+export const topLevel = Object.entries(allNodes).reduce(
+    (acc, [node, levels]) => {
+        acc.push({ value: node, image: levels.at(-1).image });
+        return acc;
+    },
+    [],
+);
