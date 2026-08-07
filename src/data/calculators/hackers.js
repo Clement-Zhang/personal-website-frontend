9f64d3b13b5c0dcae684147b691d691461884317
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-06 23:21:50 -0400

    Update hackers.js

--- src/data/calculators/hackers.js
+++ src/data/calculators/hackers.js
@@ -40,4 +40,8 @@ export const topLevel = Object.entries(allNodes)
         acc.push({ value: node, image: levels.at(-1).image });
         return acc;
     }, [])
-    .sort((before, after) => rank(before.value) - rank(after.value));
+    .sort((before, after) => {
+        console.log(before.value, after.value);
+        console.log(rank(before.value), rank(after.value));
+        return rank(before.value) - rank(after.value);
+    });
