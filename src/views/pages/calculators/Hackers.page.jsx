06e0798a6d353f87201f8b4ff5548dc27c10daef
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-14 17:37:10 -0400

    Update Hackers.page.jsx

--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -66,8 +66,8 @@ export default function Hackers() {
                     <div className="flex justify-center">
                         {nodes.map((node, index) => (
                             <div className="flex flex-col items-center">
-                                <div className="flex">
-                                    <div className="flex grow w-fit p-1 gap-x-3 border">
+                                <div className="flex grow">
+                                    <div className="flex w-fit p-1 gap-x-3 border">
                                         <div className="flex flex-col items-center">
                                             <p>Node Type</p>
                                             <ImageSelect
