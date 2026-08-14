c2cb2701d19686f1b081ee5d494dceca91de5975
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-14 17:23:01 -0400

    Update Hackers.page.jsx

--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -65,7 +65,7 @@ export default function Hackers() {
                 <Section title={side}>
                     <div className="flex justify-center">
                         {nodes.map((node, index) => (
-                            <div className="flex flex-col justify-between items-center">
+                            <div className="flex flex-col items-center">
                                 <div className="flex">
                                     <div className="flex w-fit h-full p-1 gap-x-3 border">
                                         <div className="flex flex-col items-center">
