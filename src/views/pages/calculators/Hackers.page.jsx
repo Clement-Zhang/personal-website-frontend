7c518452f9764435027feca1e56135e277bf4ee1
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-14 23:08:11 -0400

    remove button centering doesn't look at add button anymore

--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -65,61 +65,58 @@ export default function Hackers() {
                 <Section title={side}>
                     <div className="flex justify-center">
                         {nodes.map((node, index) => (
-                            <div className="flex flex-col items-center">
-                                <div className="flex grow">
-                                    <div className="flex w-fit p-1 gap-x-3 border">
-                                        <div className="flex flex-col items-center">
-                                            <p>Node Type</p>
-                                            <ImageSelect
-                                                value={node.type}
-                                                onChange={(type) => {
-                                                    state[side][index] =
-                                                        resetNode(type);
-                                                    setState({ ...state });
-                                                }}
-                                                options={topLevel[side]}
-                                            />
-                                        </div>
-                                        <div className="flex flex-col items-center">
-                                            <p>Node Level</p>
-                                            <ImageSelect
-                                                key={node.type}
-                                                value={node.level}
-                                                onChange={(level) => {
-                                                    state[side][index].level =
-                                                        level;
-                                                    setState({ ...state });
-                                                }}
-                                                options={lowLevels[node.type]}
-                                            />
-                                        </div>
+                            <div className="grid grid-cols-2 grid-rows-2">
+                                <div className="flex w-fit p-1 gap-x-3 border">
+                                    <div className="flex flex-col items-center">
+                                        <p>Node Type</p>
+                                        <ImageSelect
+                                            value={node.type}
+                                            onChange={(type) => {
+                                                state[side][index] =
+                                                    resetNode(type);
+                                                setState({ ...state });
+                                            }}
+                                            options={topLevel[side]}
+                                        />
                                     </div>
-                                    {sides[side].tags.includes('defenders') &&
-                                        node == nodes.at(-1) && (
-                                            <button
-                                                className="bg-blue-500 flex items-center"
-                                                type="button"
-                                                onClick={() => {
-                                                    state[side].push(
-                                                        resetNode(
-                                                            topLevel[side][0]
-                                                                .value,
-                                                        ),
-                                                    );
-                                                    setState({ ...state });
-                                                }}
-                                            >
-                                                <img
-                                                    src={add}
-                                                    alt="add another node"
-                                                />
-                                            </button>
-                                        )}
+                                    <div className="flex flex-col items-center">
+                                        <p>Node Level</p>
+                                        <ImageSelect
+                                            key={node.type}
+                                            value={node.level}
+                                            onChange={(level) => {
+                                                state[side][index].level =
+                                                    level;
+                                                setState({ ...state });
+                                            }}
+                                            options={lowLevels[node.type]}
+                                        />
+                                    </div>
                                 </div>
+                                {sides[side].tags.includes('defenders') &&
+                                    node == nodes.at(-1) && (
+                                        <button
+                                            className="bg-blue-500 flex w-fit items-center"
+                                            type="button"
+                                            onClick={() => {
+                                                state[side].push(
+                                                    resetNode(
+                                                        topLevel[side][0].value,
+                                                    ),
+                                                );
+                                                setState({ ...state });
+                                            }}
+                                        >
+                                            <img
+                                                src={add}
+                                                alt="add another node"
+                                            />
+                                        </button>
+                                    )}
                                 {sides[side].tags.includes('defenders') &&
                                     nodes.length > 1 && (
                                         <button
-                                            className="bg-red-500 flex items-center"
+                                            className="bg-red-500 flex items-center justify-self-center size-fit"
                                             type="button"
                                             onClick={() => {
                                                 state[side].splice(index, 1);
