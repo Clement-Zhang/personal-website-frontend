0ee41960e5492fcfe7d5772694db52e54bccdad9
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-16 17:41:52 -0400

    Update ImageSelect.jsx

--- src/views/components/customs/ImageSelect.jsx
+++ src/views/components/customs/ImageSelect.jsx
@@ -7,6 +7,7 @@ export default function ImageSelect({ value, onChange, options }) {
     const [selected, setSelected] = useState(
         options.find((option) => option.value == value),
     );
+    console.log(options);
     return (
         <div className="relative">
             <button
