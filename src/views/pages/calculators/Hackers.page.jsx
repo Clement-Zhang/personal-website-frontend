d612c341c340d57618f409a4bc7f02c29af2aac1
Clement-Zhang <zhangclement947@yahoo.ca>
2026-08-17 22:42:50 -0400

    Update Hackers.page.jsx

--- src/views/pages/calculators/Hackers.page.jsx
+++ src/views/pages/calculators/Hackers.page.jsx
@@ -156,7 +156,7 @@ export default function Hackers() {
                                                     setLoadout([...loadout]);
                                                 }}
                                                 options={programs}
-                                                width="w-10"
+                                                width="w-9"
                                             />
                                         ))}
                                     </div>
